7d07b51a4a848083e1407adf10680e1ed9898eff
Alejandro Casado Quijada <acasadoquijada@gmail.com>
2017-07-26 20:49:25 +0200

    Avances en #2

--- JMR-Android/app/src/main/java/com/example/alejandro/jmr_android/Galeria.java
+++ JMR-Android/app/src/main/java/com/example/alejandro/jmr_android/Galeria.java
@@ -3,12 +3,15 @@ package com.example.alejandro.jmr_android;
 import android.app.Activity;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.style.DrawableMarginSpan;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -19,6 +22,7 @@ public class Galeria extends AppCompatActivity {
     /** The images. */
     private ArrayList<String> images;
     private ImageView imageView;
+    private LinearLayout imageLayout;
     String a;
 
     @Override
@@ -27,9 +31,9 @@ public class Galeria extends AppCompatActivity {
         setContentView(R.layout.activity_galeria);
 
         imageView = (ImageView) findViewById(R.id.imageView);
-
+        imageLayout = (LinearLayout) findViewById(R.id.imageLayout);
         images = getAllShownImagesPath(this);
-        Log.i("PepitoPerez", images.get(0));
+        Log.i("PepitoPerez", Integer.toString(images.size()));
 
         Bitmap bm=null;
         try {
@@ -40,7 +44,31 @@ public class Galeria extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        Bitmap bm2=null;
+        try {
+            bm2 = MediaStore.Images.Media.getBitmap(
+                    getApplicationContext().getContentResolver(),
+                    Uri.parse("file:///" + images.get(2)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         imageView.setImageBitmap(bm);
+        ArrayList<ImageView> imagenesView = new ArrayList<>();
+
+        for(int i = 0; i < 10; i++){
+            ImageView imageG = new ImageView (this);
+            imageG.setImageBitmap(bm2);
+            imageLayout.addView(imageG);
+            DrawableMarginSpan divider = new DrawableMarginSpan();
+
+        }
+
+
+       // imageLayout.addView(imageView);
+
+
+
 
 
 
