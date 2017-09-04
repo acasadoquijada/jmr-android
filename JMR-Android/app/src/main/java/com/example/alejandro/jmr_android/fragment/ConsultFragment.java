7c86ad57c23cd671b6f775ae80d0d25cc54f7ef7
Alejandro Casado Quijada <acasadoquijada@gmail.com>
2017-09-04 15:39:00 +0200

    Capturar imagen desde la cámara #1

--- JMR-Android/app/src/main/java/com/example/alejandro/jmr_android/fragment/ConsultFragment.java
+++ JMR-Android/app/src/main/java/com/example/alejandro/jmr_android/fragment/ConsultFragment.java
@@ -5,10 +5,20 @@ package com.example.alejandro.jmr_android.fragment;
  */
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Matrix;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -21,36 +31,46 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.alejandro.jmr_android.Gallery;
 import com.example.alejandro.jmr_android.R;
+import com.example.alejandro.jmr_android.Utility;
 import com.example.alejandro.jmr_android.activity.MainActivity;
 import com.example.alejandro.jmr_android.adapter.GalleryAdapter;
 import com.example.alejandro.jmr_android.jmr.ResultList;
 import com.example.alejandro.jmr_android.jmr.ResultMetadata;
 import com.example.alejandro.jmr_android.model.Image;
+import com.github.clans.fab.FloatingActionButton;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import it.sephiroth.android.library.bottomnavigation.BottomNavigation;
 
 public class ConsultFragment extends Fragment {
 
-    private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
+    private static final int CAMERA_REQUEST = 1888;
     final private int REQUEST_MEDIA_ACCESS = 2;
     private String userChoosenTask;
     private Gallery imagenesGaleria;
     private Bitmap imagenConsulta;
     private Color colorImagenConsulta;
     private ResultList<ResultMetadata> resultMetadatas;
-    private ArrayList<Image> images;
+    private ArrayList<Image> images, imageConsulta;
     private ProgressDialog pDialog;
     private GalleryAdapter mAdapter, mAdapter2;
     private RecyclerView recyclerView, recyclerView2;
-    private BottomNavigation bottomNavigation;
-    private ConsultFragment consultFragment;
-    private SettingFragment settingFragment;
-    private StatisticsFragment statisticsFragment;
+    private FloatingActionButton fab_camera;
+    private Uri mImageUri;
+
 
     public static ConsultFragment newInstance() {
         ConsultFragment fragment = new ConsultFragment();
@@ -62,6 +82,7 @@ public class ConsultFragment extends Fragment {
         super.onCreate(savedInstanceState);
         imagenesGaleria = ((MainActivity) getActivity()).getGallery();
         images = new ArrayList<>();
+        imageConsulta = new ArrayList<>();
         colocarImagenesResultado();
     }
 
@@ -69,7 +90,6 @@ public class ConsultFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_consult, container, false);
-
         return view;
     }
 
@@ -77,15 +97,111 @@ public class ConsultFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         setConsultImage();
         setResultImage();
+
+        fab_camera = (FloatingActionButton) getView().
+                findViewById(R.id.menu_item_camera);
+
+        fab_camera.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                boolean result= Utility.checkPermission(getContext());
+
+                if(result){
+                    cameraIntent();
+                }
+
+            }
+        });
+
         super.onViewCreated(view, savedInstanceState);
     }
 
-    @Override
-    public void setUserVisibleHint(boolean isVisibleToUser) {
-
-        Log.d("SOY", "VISIBLE");
-
+    private File createTemporaryFile(String part, String ext) throws Exception {
+        File tempDir= Environment.getExternalStorageDirectory();
+        tempDir=new File(tempDir.getAbsolutePath()+"/.temp/");
+        if(!tempDir.exists())
+        {
+            tempDir.mkdirs();
+        }
+        return File.createTempFile(part, ext, tempDir);
     }
+
+    private void cameraIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        File photo = null;
+        try
+        {
+            // place where to store camera taken picture
+            photo = this.createTemporaryFile("picture", ".jpg");
+            photo.delete();
+        }
+        catch(Exception e) {
+
+        }
+
+        mImageUri = Uri.fromFile(photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
+
+        startActivityForResult(intent, CAMERA_REQUEST);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CAMERA_REQUEST)
+                onCaptureImageResult(data);
+        }
+    }
+
+    private void onCaptureImageResult(Intent data) {
+        getContext().getContentResolver().notifyChange(mImageUri, null);
+        ContentResolver cr = getContext().getContentResolver();
+        Bitmap bitmap;
+        try
+        {
+            bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, mImageUri);
+
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+
+
+            File destination = new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DCIM).getPath() + "/Camera",
+
+                    System.currentTimeMillis() + ".jpg");
+
+            FileOutputStream fo;
+            try {
+                destination.createNewFile();
+                fo = new FileOutputStream(destination);
+                fo.write(bytes.toByteArray());
+                fo.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } catch (Exception e) {
+            Log.d("Failed to load", e.toString());
+        }
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case Utility.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    cameraIntent();
+
+                } else {
+                    //code for deny
+                }
+                break;
+        }
+    }
+
     private void setConsultImage() {
 
         /*
@@ -172,7 +288,6 @@ public class ConsultFragment extends Fragment {
                             }
                         }));
     }
-
 
     public void colocarImagenesResultado(){
 
